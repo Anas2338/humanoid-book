9f0749b8b81080e12757e5649405a9d708802789
Anas2338 <mohdanus20@gmail.com>
2025-12-13 16:41:24 +0500

    Add trailingSlash option to fix GitHub Pages asset loading

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,6 +15,7 @@ const config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/humanoid-book/',
+  trailingSlash: true,
 
   // GitHub pages deployment config.
   organizationName: 'Anas2338', // Usually your GitHub org/user name
